b0275e671cc4d41daac4eeaa9588c5f11e4a607c
Rafael Monteiro <diatosta123@gmail.com>
2019-01-25 12:37:04 +0000

    Voting data is now submitted successfully to the database.

--- BinCompeteSoftUWP/Pages/VotingPage.xaml.cs
+++ BinCompeteSoftUWP/Pages/VotingPage.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -100,6 +101,81 @@ namespace BinCompeteSoftUWP.Pages
                 CriteriasListView.ItemsSource = criteriaList;
             }
         }
+
+        /// <summary>
+        /// Inserts all the voting data for this contest in the database.
+        /// </summary>
+        private async Task<bool> InsertVotingDataIntoDB()
+        {
+            try
+            {
+                string query = "INSERT INTO contest_juri_criteria_table " +
+                    "VALUES (@id_contest, @id_user, @id_criteria1, @id_criteria2, @value)";
+
+                SqlCommand cmd;
+
+                // Cycle through all criterias.
+                foreach (CriteriaList criteria in criteriaList)
+                { 
+                    cmd = DBSqlHelper.Connection.CreateCommand();
+                    cmd.CommandText = query;
+
+                    cmd.Parameters.Add(new SqlParameter("@id_contest", ContestToEdit.Id));
+
+                    cmd.Parameters.Add(new SqlParameter("@id_user", Data.Instance.LoggedInUser.Id));
+
+                    cmd.Parameters.Add(new SqlParameter("@id_criteria1", criteria.Criteria1.Id));
+
+                    cmd.Parameters.Add(new SqlParameter("@id_criteria2", criteria.Criteria2.Id));
+
+                    cmd.Parameters.Add(new SqlParameter("@value", criteria.Value));
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+
+                query = "INSERT INTO evaluation_table " +
+                    "VALUES (@id_user, @id_contest, @id_criteria, @id_project1, @id_project2, @value)";
+
+                // Cycle through all projects.
+                foreach(CriteriaProjectsList criteriaProjects in criteriaProjectsList)
+                {
+                    cmd = DBSqlHelper.Connection.CreateCommand();
+                    cmd.CommandText = query;
+
+                    cmd.Parameters.Add(new SqlParameter("@id_contest", ContestToEdit.Id));
+
+                    cmd.Parameters.Add(new SqlParameter("@id_user", Data.Instance.LoggedInUser.Id));
+
+                    cmd.Parameters.Add(new SqlParameter("@id_criteria", criteriaProjects.Criteria.Id));
+
+                    cmd.Parameters.Add(new SqlParameter("@id_project1", criteriaProjects.Project1.Id));
+
+                    cmd.Parameters.Add(new SqlParameter("@id_project2", criteriaProjects.Project2.Id));
+
+                    cmd.Parameters.Add(new SqlParameter("@value", criteriaProjects.Value));
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+
+                // Update the user voted status.
+                query = "UPDATE contest_juri_table SET has_voted = 1 WHERE id_contest = @id_contest AND id_user = @id_user";
+
+                cmd = DBSqlHelper.Connection.CreateCommand();
+                cmd.CommandText = query;
+
+                cmd.Parameters.Add(new SqlParameter("@id_contest", ContestToEdit.Id));
+
+                cmd.Parameters.Add(new SqlParameter("@id_user", Data.Instance.LoggedInUser.Id));
+
+                await cmd.ExecuteNonQueryAsync();
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
         #endregion
 
         #region Class event handlers
@@ -119,6 +195,82 @@ namespace BinCompeteSoftUWP.Pages
             List<CriteriaProjectsList> projectsList = criteriaProjectsList.FindAll(criteria => criteria.Criteria.Id == selectedCriteria.Id);
 
             ProjectsListView.ItemsSource = projectsList;
+        }
+
+        private async void SendVoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            OverlayRectangle.Visibility = Visibility.Visible;
+            InsertVoteProgressRing.IsActive = true;
+            InsertVoteTextBlock.Visibility = Visibility.Visible;
+
+            if(await InsertVotingDataIntoDB())
+            {
+                ContentDialog contentMsg = new ContentDialog
+                {
+                    Title = "Success",
+                    Content = "Updated contest successfully.",
+                    PrimaryButtonText = "OK"
+                };
+
+                App.ShowContentDialog(contentMsg, null);
+
+                this.Frame.Navigate(typeof(JudgeDashboardPage));
+            }
+            else
+            {
+                OverlayRectangle.Visibility = Visibility.Collapsed;
+                InsertVoteProgressRing.IsActive = false;
+                InsertVoteTextBlock.Visibility = Visibility.Collapsed;
+
+                ContentDialog errorMsg = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = "Error updating the contest, try again later.",
+                    PrimaryButtonText = "OK"
+                };
+
+                App.ShowContentDialog(errorMsg, null);
+            }
+        }
+
+        private void CancelVoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog contentDialog = new ContentDialog
+            {
+                Title = "Cancel?",
+                Content = "Do you really wish to stop voting for this contest?",
+                PrimaryButtonText = "Yes",
+                SecondaryButtonText = "No"
+            };
+
+            // Create callback to be called when ContentDialog closes.
+            Action<ContentDialogResult> callback = (result) =>
+            {
+                if (result == ContentDialogResult.Primary)
+                {
+                    Frame.GoBack();
+                }
+            };
+
+            App.ShowContentDialog(contentDialog, callback);
+        }
+
+        private void ProjectImportanceSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            // Check if value is 0.
+            if(e.NewValue == 0)
+            {
+                ((Slider)sender).Value = e.OldValue;
+            }
+        }
+
+        private void CriteriaImportanceSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            // Check if value is 0.
+            if (e.NewValue == 0)
+            {
+                ((Slider)sender).Value = e.OldValue;
+            }
         }
         #endregion
     }
